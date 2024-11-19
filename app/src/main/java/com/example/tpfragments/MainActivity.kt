8a5692e7d82ee3ab8f97984a70534dd35567b4c6
promanenko <paihitetty444@gmail.com>
2024-11-19 16:40:25 +0100

    La creation des fragments

--- app/src/main/java/com/example/tpfragments/MainActivity.kt
+++ app/src/main/java/com/example/tpfragments/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 //        binding.numberField.setText("0")
 //
 //        binding.buttonPlus.setOnClickListener {
